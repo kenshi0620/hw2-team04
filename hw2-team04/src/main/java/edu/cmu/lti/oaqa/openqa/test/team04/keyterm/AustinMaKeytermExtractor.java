aca84c1b863b0081d282344470722d15f5659e13
armatthe <armatthe@ubuntu>
2012-11-07 15:16:00 -0800

    Finally got my code working!!

--- hw2-team04/src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/keyterm/AustinMaKeytermExtractor.java
+++ hw2-team04/src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/keyterm/AustinMaKeytermExtractor.java
@@ -1,5 +1,7 @@
 package edu.cmu.lti.oaqa.openqa.test.team04.keyterm;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,24 +13,51 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.Chunker;
+import com.aliasi.chunk.Chunking;
+import com.aliasi.util.AbstractExternalizable;
+
 import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
 
 public class AustinMaKeytermExtractor extends AbstractKeytermExtractor {
 
+  /**
+   * File location of NER model
+   */
+  File nerModelFile;
+  
+  /**
+   * Ling pipe chunker that will do the NER for us
+   */
+  Chunker chunker;
+  
   @Override
   public void initialize(UimaContext c) throws ResourceInitializationException {
     super.initialize(c);
+    
+    nerModelFile = new File("src/main/resources/ne-en-bio-genetag.HmmChunker");
+    try {
+      chunker = (Chunker) AbstractExternalizable.readObject(nerModelFile);
+    } catch (IOException e) {
+      System.err.println("IOException in creating chunker");
+      throw new ResourceInitializationException("Unable to load NER model file", "load_ner_model_error",
+              new Object[] { nerModelFile }, e);
+    } catch (ClassNotFoundException e) {
+      System.err.println("ClassNotFoundException in creating chunker");
+      throw new ResourceInitializationException("Unable to load NER model file", "load_ner_model_error",
+              new Object[] { nerModelFile }, e);
+    }
   }
 
   @Override
   protected List<Keyterm> getKeyterms(String text) {
-    Map<Integer, Integer> geneSpans = new HashMap<Integer, Integer>(); // ner.getGeneSpans(text);
     List<Keyterm> keyTerms = new ArrayList<Keyterm>();
-
-    for (Entry<Integer, Integer> entry : geneSpans.entrySet()) {
-      int start = entry.getKey();
-      int end = entry.getValue();
+    Chunking chunking = chunker.chunk(text);
+    for (Chunk chunk : chunking.chunkSet()) {
+      int start = chunk.start();
+      int end = chunk.end();
       String term = text.substring(start, end);
       keyTerms.add(new Keyterm(term));
     }
