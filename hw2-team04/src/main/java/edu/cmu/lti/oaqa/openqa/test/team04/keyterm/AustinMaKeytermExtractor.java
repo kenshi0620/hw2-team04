e113f69b570ba486bfa34589c99db753c18b1390
armatthe <armatthe@ubuntu>
2012-11-06 22:48:37 -0800

    Fixed error

--- hw2-team04/src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/keyterm/AustinMaKeytermExtractor.java
+++ hw2-team04/src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/keyterm/AustinMaKeytermExtractor.java
@@ -1,6 +1,7 @@
 package edu.cmu.lti.oaqa.openqa.test.team04.keyterm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -12,7 +13,6 @@ import org.apache.uima.resource.ResourceInitializationException;
 
 import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
-import edu.cmu.lti.oaqa.openqa.test.team04.keyterm.austinma.PosTagNamedEntityRecognizer;
 
 public class AustinMaKeytermExtractor extends AbstractKeytermExtractor {
 
@@ -23,20 +23,15 @@ public class AustinMaKeytermExtractor extends AbstractKeytermExtractor {
 
   @Override
   protected List<Keyterm> getKeyterms(String text) {
-    try {
-      PosTagNamedEntityRecognizer ner = new PosTagNamedEntityRecognizer();
-      Map<Integer, Integer> geneSpans = ner.getGeneSpans(text);
-      List<Keyterm> keyTerms = new ArrayList<Keyterm>();
-      
-      for(Entry<Integer, Integer> entry : geneSpans.entrySet()) {
-        int start = entry.getKey();
-        int end = entry.getValue();
-        String term = text.substring(start, end);
-        keyTerms.add(new Keyterm(term));
-      }
-      return keyTerms;
-    } catch (ResourceInitializationException e) {
-      return null;
+    Map<Integer, Integer> geneSpans = new HashMap<Integer, Integer>(); // ner.getGeneSpans(text);
+    List<Keyterm> keyTerms = new ArrayList<Keyterm>();
+
+    for (Entry<Integer, Integer> entry : geneSpans.entrySet()) {
+      int start = entry.getKey();
+      int end = entry.getValue();
+      String term = text.substring(start, end);
+      keyTerms.add(new Keyterm(term));
     }
+    return keyTerms;
   }
 }
