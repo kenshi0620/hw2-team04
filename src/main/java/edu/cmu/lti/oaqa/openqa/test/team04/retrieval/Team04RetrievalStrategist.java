97f930da0fd16f49ab4909894f4581e658238c2d
afandria <afandria@ADVERB.lti.cs.cmu.edu>
2012-11-27 19:52:19 -0500

    retrieval: term grouping, stem + wildcard, ranking, # set to 25, etc

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/retrieval/Team04RetrievalStrategist.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/retrieval/Team04RetrievalStrategist.java
@@ -1,56 +1,101 @@
 package edu.cmu.lti.oaqa.openqa.test.team04.retrieval;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.tartarus.martin.Stemmer;
 
+import edu.cmu.lti.oaqa.core.provider.solr.SolrWrapper;
+import edu.cmu.lti.oaqa.cse.basephase.retrieval.AbstractRetrievalStrategist;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
 import edu.cmu.lti.oaqa.framework.data.RetrievalResult;
-import edu.cmu.lti.oaqa.openqa.hello.retrieval.SimpleSolrRetrievalStrategist;
+import edu.washington.cs.knowitall.morpha.MorphaStemmer;
 
-public class Team04RetrievalStrategist extends SimpleSolrRetrievalStrategist {
+public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
+
+  protected Integer hitListSize;
+
+  protected SolrWrapper wrapper;
+
+  @Override
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
+    try {
+      this.hitListSize = (Integer) aContext.getConfigParameterValue("hit-list-size");
+    } catch (ClassCastException e) { // all cross-opts are strings?
+      this.hitListSize = Integer.parseInt((String) aContext
+              .getConfigParameterValue("hit-list-size"));
+    }
+    String serverUrl = (String) aContext.getConfigParameterValue("server");
+    Integer serverPort = (Integer) aContext.getConfigParameterValue("port");
+    Boolean embedded = (Boolean) aContext.getConfigParameterValue("embedded");
+    String core = (String) aContext.getConfigParameterValue("core");
+    try {
+      this.wrapper = new SolrWrapper(serverUrl, serverPort, embedded, core);
+    } catch (Exception e) {
+      throw new ResourceInitializationException(e);
+    }
+  }
+
+  @Override
+  protected final List<RetrievalResult> retrieveDocuments(String questionText,
+          List<Keyterm> keyterms) {
+    String query = formulateQuery(questionText, keyterms);
+    return retrieveDocuments(query);
+  };
+
+  @Override
+  public void collectionProcessComplete() throws AnalysisEngineProcessException {
+    super.collectionProcessComplete();
+    wrapper.close();
+  }
 
   protected List<RetrievalResult> retrieveDocuments(String queryString) {
     List<RetrievalResult> result = new ArrayList<RetrievalResult>();
     try {
+      System.out.println(" QQ: " + queryString);
       SolrQuery query = new SolrQuery();
       query.setQuery(wrapper.escapeQuery(queryString));
       query.setRows(hitListSize);
       query.setFields("*", "score");
       SolrDocumentList docs = wrapper.runQuery(query, hitListSize);
-
+      int i = 0;
       for (SolrDocument doc : docs) {
-
         RetrievalResult r = new RetrievalResult((String) doc.getFieldValue("id"),
                 (Float) doc.getFieldValue("score"), queryString);
+        r.setRank(i++);
         result.add(r);
-        System.out.println(" SUP DOC: " + doc.getFieldValue("id"));
+        System.out.println(" SUP DOC: " + doc.getFieldValue("id") + " ranked at " + r.getRank()
+                + " or " + (Float) doc.getFieldValue("score"));
       }
     } catch (Exception e) {
       System.err.println("Error retrieving documents from Solr: " + e);
     }
     return result;
   }
-  
-  protected String formulateQuery(List<Keyterm> keyterms) {
+
+  protected String formulateQuery(String questionText, List<Keyterm> keyterms) {
+    // SpellChecker sc = new CompiledSpellChecker(null, null, null);
     StringBuffer result = new StringBuffer();
     for (Keyterm keyterm : keyterms) {
-      result.append(keyterm.getText() + " ");
       System.out.println(" TRANSFORM: " + keyterm.getText() + " --> " + stem(keyterm.getText()));
+      // System.out.println(" TRANSFORM: " + keyterm.getText() + " --> "
+      // + MorphaStemmer.stemToken(keyterm.getText()));
+      result.append("\"" + stem(keyterm.getText()) + "*\" ");
     }
+    result.append(questionText);
     String query = result.toString();
     System.out.println(" QUEREAL: " + query);
     return query;
   }
-  
-  private String stem(String s)
-  {
+
+  private String stem(String s) {
     Stemmer stemmer = new Stemmer();
     stemmer.add(s.toCharArray(), s.length());
     stemmer.stem();
