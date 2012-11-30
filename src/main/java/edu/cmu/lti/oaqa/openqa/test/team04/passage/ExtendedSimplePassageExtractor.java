b2ee7dd6bc7b44297e85a8b5f07a7d4c441e3c17
kenshi0620 <weijiazhang28@gmail.com>
2012-11-30 00:06:40 -0500

    improved passage extractor - fixed offset bug - add new online geneName checker

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/passage/ExtendedSimplePassageExtractor.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/passage/ExtendedSimplePassageExtractor.java
@@ -1,20 +1,14 @@
 package edu.cmu.lti.oaqa.openqa.test.team04.passage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.jsoup.Jsoup;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
 import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
 import edu.cmu.lti.oaqa.framework.data.RetrievalResult;
-import edu.cmu.lti.oaqa.openqa.hello.passage.KeytermWindowScorerSum;
-import edu.cmu.lti.oaqa.openqa.hello.passage.PassageCandidateFinder;
 import edu.cmu.lti.oaqa.openqa.hello.passage.SimplePassageExtractor;
+import edu.cmu.lti.oaqa.openqa.test.team04.passage.basic.KeytermListExtendor;
 import edu.cmu.lti.oaqa.openqa.test.team04.passage.finder.ExtendedSimplePassageCandidateFinder;
 
 public class ExtendedSimplePassageExtractor extends SimplePassageExtractor {
@@ -22,6 +16,14 @@ public class ExtendedSimplePassageExtractor extends SimplePassageExtractor {
   @Override
   protected List<PassageCandidate> extractPassages(String question, List<Keyterm> keyterms,
           List<RetrievalResult> documents) {
+    
+    KeytermListExtendor myKLE = new  KeytermListExtendor(); 
+    try {
+      keyterms = myKLE.KeytermListExtendor(keyterms);
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+    
     if (keyterms.size() == 0 || documents.size() == 0)
       return new ArrayList<PassageCandidate>();
 
--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/passage/finder/ExtendedSimplePassageCandidateFinder.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/passage/finder/ExtendedSimplePassageCandidateFinder.java
@@ -1,5 +1,7 @@
 package edu.cmu.lti.oaqa.openqa.test.team04.passage.finder;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,9 +23,8 @@ import edu.cmu.lti.oaqa.core.provider.solr.SolrWrapper;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
 import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
 import edu.cmu.lti.oaqa.framework.data.RetrievalResult;
-import edu.cmu.lti.oaqa.openqa.hello.passage.KeytermWindowScorer;
-import edu.cmu.lti.oaqa.openqa.hello.passage.PassageCandidateFinder;
 import edu.cmu.lti.oaqa.openqa.test.team04.passage.basic.IdfIndexer;
+import edu.cmu.lti.oaqa.openqa.test.team04.passage.finder.Team04PassageCandidateFinder.PassageSpan;
 
 public class ExtendedSimplePassageCandidateFinder {
   private RealMatrix p2dMatrix;
@@ -45,10 +46,10 @@ public class ExtendedSimplePassageCandidateFinder {
     for (Keyterm keyterm: keyterms) {
       keytermList.add(keyterm.getText());
     }
-
+    
     this.numDocs = documents.size();
     this.numWords = keyterms.size();
-
+    
     double[][] data = new double[numWords][numDocs];
     docTexts = new ArrayList<String>();
     this.documents = documents;
@@ -60,29 +61,46 @@ public class ExtendedSimplePassageCandidateFinder {
     for (RetrievalResult document : documents) {
       String key = document.getDocID();
       String text = getText(key);
-      Map<Integer, Boolean> leftMap = new HashMap<Integer, Boolean>();
-      Map<Integer, Boolean> rightMap = new HashMap<Integer, Boolean>();
+  
+      /*
+       * get output the document to see the html files
+      String OutputDocumentName = "/Users/zhangweijia/Documents/" + docCnt;
+      File output = new File (OutputDocumentName);
+      FileWriter fileWriter = new FileWriter(output);
+      fileWriter.write(text);
+      fileWriter.flush();
+      */
+  
+      Map<Integer, Boolean> leftMap = new HashMap<Integer, Boolean>(); // all keywords start position
+      Map<Integer, Boolean> rightMap = new HashMap<Integer, Boolean>(); // all keywords end position
+      // the same key of the two map above belongs to the same keyword.
+      
       docTexts.add(text);
-
-      Map<String, List<PassageSpan>> keyMap = new HashMap<String, List<PassageSpan>>();     
-      int keyId = 0;
+      
+      Map<String, List<PassageSpan>> keyMap = new HashMap<String, List<PassageSpan>>();
+      // store the location of each passage in the document
+      
+      int keyId = 0; // keyterm ID
       for (String keyterm: keytermList) {
         List<PassageSpan> spanList = new ArrayList<PassageSpan>();
+        // passage span?? a list of certain keyterm position (start&end)
         Pattern p = Pattern.compile(keyterm);
         Matcher m = p.matcher(text);
         while ( m.find() ) {
           leftMap.put(m.start(), true);
           rightMap.put(m.end(), true);
-          spanList.add(new PassageSpan(m.start() , m.end()));
+          spanList.add(new PassageSpan(m.start() , m.end())); // add every keyterm in spanlist
         }
         data[keyId][docCnt] = (double)spanList.size();
         keyMap.put(keyterm, spanList);
         keyId++;
       }
-      leftEdgesDocs.add(new ArrayList<Integer>(leftMap.keySet()));
-      rightEdgesDocs.add(new ArrayList<Integer>(rightMap.keySet()));
-
-      p2dMap.add(keyMap);
+      
+      leftEdgesDocs.add(new ArrayList<Integer>(leftMap.keySet())); // all keyterms start points in this document
+      rightEdgesDocs.add(new ArrayList<Integer>(rightMap.keySet())); // all keyterms end points in this document
+      // the same key of 2 para above belongs to same keyterm
+      
+      p2dMap.add(keyMap);// all keyterm and its position, no document info
       docCnt++;
     }
     RealMatrix dataCnt = new RealMatrixImpl(data);
@@ -95,10 +113,10 @@ public class ExtendedSimplePassageCandidateFinder {
     // For every possible window, calculate keyterms found, matches found; score window, and create passage candidate.
     for (int i = 0; i < numDocs; i++) {
       // create set of left edges and right edges which define possible windows.
-      List<Integer> leftEdges = leftEdgesDocs.get(i);
-      List<Integer> rightEdges = rightEdgesDocs.get(i);
-      String text = docTexts.get(i);
-      int textSize = text.length();
+      List<Integer> leftEdges = leftEdgesDocs.get(i); // keyterm start point
+      List<Integer> rightEdges = rightEdgesDocs.get(i); // keytrm end point
+      String text = docTexts.get(i); // keyterm string
+      int textSize = text.length(); // keyterm size
       
       // calculate every total matches and total keyterms in the whole document text
       double totalMatches = 0;
@@ -128,6 +146,8 @@ public class ExtendedSimplePassageCandidateFinder {
           int keytermsFound = 0;
           
           String passageText = text.substring(begin, end);
+          String cleanPassageText = Jsoup.parse(passageText).text().replaceAll("([\177-\377\0-\32]*)", "");
+          
           int keyId = 0;
           for (String keyterm: keytermList) {
             List<PassageSpan> spans = spanMap.get(keyterm);
@@ -141,11 +161,20 @@ public class ExtendedSimplePassageCandidateFinder {
             if (exist) keytermsFound++;
             keyId++;
           }
-          int windowSize = end - begin;
+          int windowSize = cleanPassageText.length();
           double offsetScore = ( (double)textSize - (double)begin ) / (double)textSize;
           double score = .25d *( (double)matchesFound / (double)totalMatches ) 
           + .25d * ( (double)keytermsFound / (double)totalKeyterms) 
           + .25d * ( 1 - ( (double)windowSize / (double)textSize ) + .25d * offsetScore );
+          
+          /*
+           * get output the document to see the html files
+          String OutputDocumentName = "/Users/zhangweijia/Documents/" + docCnt;
+          File output = new File (OutputDocumentName);
+          FileWriter fileWriter = new FileWriter(output);
+          fileWriter.write(text);
+          fileWriter.flush();
+          */
           
           PassageCandidate window = null;
           try {
@@ -182,8 +211,18 @@ public class ExtendedSimplePassageCandidateFinder {
           String htmlText = wrapper.getDocText(id);
 
           // cleaning HTML text
-          text = Jsoup.parse(htmlText).text().replaceAll("([\177-\377\0-\32]*)", "")/* .trim() */;
+          //text = Jsoup.parse(htmlText).text().replaceAll("([\177-\377\0-\32]*)", "")/* .trim() */;
           // for now, making sure the text isn't too long
+          
+          // try to clean the text by starting from the first passage in the document
+          Pattern p = Pattern.compile( "<P>" );
+          Matcher m = p.matcher( text );
+          if ( m.find() ) {
+            text = text.substring(m.start());
+          }
+          
+          
+          text = htmlText;
           text = text.substring(0, Math.min(5000, text.length()));
         } catch (SolrServerException e) {
           e.printStackTrace();
