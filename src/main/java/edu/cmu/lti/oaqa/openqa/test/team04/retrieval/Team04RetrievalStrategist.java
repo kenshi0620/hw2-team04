cafb32ed2ef26ebff12b6f332d10159f23701a20
afandria <afandria@ADVERB.lti.cs.cmu.edu>
2012-12-10 21:19:49 -0500

    updating with javadoc and parameterization

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/retrieval/Team04RetrievalStrategist.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team04/retrieval/Team04RetrievalStrategist.java
@@ -33,31 +33,78 @@ import edu.cmu.lti.oaqa.openqa.test.team04.keyterm.afandria.AnnotationUtilities;
 import edu.cmu.lti.oaqa.openqa.test.team04.keyterm.afandria.PosTagNamedEntityRecognizer;
 import edu.cmu.lti.oaqa.openqa.test.team04.passage.basic.KeytermListExtendor;
 
+/**
+ * @author afandria
+ * 
+ *         Team 04's Document Retrieval Strategist, written by afandria. We do query expansion by
+ *         finding synonyms of keyterms using Wikipedia redirect (courtesy of Yibin Lin's group) and
+ *         Big Huge Thesaurus. We also support stemming, wilcarding, overriding, and grouping query
+ *         terms. There are more details below. To reduce load on the passage extractor, we do
+ *         pre-passage extraction filtering of documents using a loose match probability
+ *         enhancement.
+ */
 public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
 
+  /**
+   * The factor to boost a document's probability by if a loose match occurs. It doesn't matter if
+   * document_prob * (LOOSE_MATCH_BOOST ^ n) > 1.0
+   */
+  private static final double LOOSE_MATCH_BOOST = 1.5;
+
+  /**
+   * Stem keyterms if this flag is set
+   */
   private static boolean STEM = false;
 
+  /**
+   * Override keyterms with POS tagged entities if this flag is set
+   */
   private static boolean OVERRIDE_KEYTERMS_WITH_NN = false;
 
+  /**
+   * Use wildcards on the ends of keyterms if this flag is set
+   */
+  protected static boolean ASTERISK = false;
+
+  /**
+   * Minimum length for a single keyterm (excludes stuff like AS, but will check for things like L2)
+   */
   private static int MINIMUM_KEYTERM_LENGTH = 2;
 
+  /**
+   * How many documents to retrieve from SOLR
+   */
   protected Integer hitListSize;
 
+  /**
+   * How many documents to return to passage extractor
+   */
   protected Integer hitListFinal;
 
-  protected boolean asterisk;
-
+  /**
+   * This is the wrapper for SOLR
+   */
   protected SolrWrapper wrapper;
 
+  /**
+   * This is our NER
+   */
   protected PosTagNamedEntityRecognizer ner;
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see edu.cmu.lti.oaqa.ecd.log.AbstractLoggedComponent#initialize(org.apache.uima.UimaContext)
+   */
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
     super.initialize(aContext);
     try {
       this.hitListSize = (Integer) aContext.getConfigParameterValue("hit-list-size");
       this.hitListFinal = (Integer) aContext.getConfigParameterValue("hit-list-final");
-      this.asterisk = (Boolean) aContext.getConfigParameterValue("asterisk");
+      this.ASTERISK = (Boolean) aContext.getConfigParameterValue("asterisk");
+      this.STEM = (Boolean) aContext.getConfigParameterValue("stem");
+      this.OVERRIDE_KEYTERMS_WITH_NN = (Boolean) aContext.getConfigParameterValue("override");
     } catch (ClassCastException e) { // all cross-opts are strings?
       this.hitListSize = Integer.parseInt((String) aContext
               .getConfigParameterValue("hit-list-size"));
@@ -76,22 +123,42 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     }
   }
 
+  /**
+   * This function will return matching documents based on keyterms and the question.
+   * 
+   * @param questionText
+   *          question text from user
+   * @param keyterms
+   *          keyterms from keyterm extractor
+   * @return list of retrieval results
+   */
   @Override
   protected final List<RetrievalResult> retrieveDocuments(String questionText,
           List<Keyterm> keyterms) {
+    // this ignores keyterms parameter
     if (OVERRIDE_KEYTERMS_WITH_NN) {
       keyterms = getNNKeyterm(questionText);
     }
+    // first get the SOLR query string
     String query = formulateQuery(questionText, keyterms);
+    // then filter the documents and return only 'hitListFinal' amount of documents
     return filterDocuments(questionText, keyterms, retrieveDocuments(query), hitListFinal);
   };
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.uima.analysis_component.AnalysisComponent_ImplBase#collectionProcessComplete()
+   */
   @Override
   public void collectionProcessComplete() throws AnalysisEngineProcessException {
     super.collectionProcessComplete();
     wrapper.close();
   }
 
+  /**
+   * This compares retrieval results and returns a rank order
+   */
   private static Comparator<RetrievalResult> RetrievalResultComparator = new Comparator<RetrievalResult>() {
     public int compare(RetrievalResult x, RetrievalResult y) {
       if (x.getProbability() < y.getProbability()) {
@@ -104,6 +171,9 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     }
   };
 
+  /**
+   * This compares text spans based on its start position. Ties are not broken.
+   */
   private static Comparator<Map.Entry<Integer, Integer>> SpanComparator = new Comparator<Map.Entry<Integer, Integer>>() {
     public int compare(Map.Entry<Integer, Integer> x, Map.Entry<Integer, Integer> y) {
       if (x.getKey() > y.getKey()) {
@@ -116,6 +186,14 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     }
   };
 
+  /**
+   * This function uses the NER to get POS tagged entities in our question text. This is meant to
+   * override the keyterm extractor in hopes of better performance.
+   * 
+   * @param questionText
+   *          question text from user
+   * @return a list of POS tagged keyterms
+   */
   private List<Keyterm> getNNKeyterm(String questionText) {
     // initialize list
     List<Keyterm> keyterms = new ArrayList<Keyterm>();
@@ -126,11 +204,20 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     while (spanIterator.hasNext()) {
       Map.Entry<Integer, Integer> me = spanIterator.next();
       keyterms.add(new Keyterm(questionText.substring(me.getKey(), me.getValue())));
-      System.out.println(" NNK: " + questionText.substring(me.getKey(), me.getValue()));
+      System.out.println(" NNKeyterm: " + questionText.substring(me.getKey(), me.getValue()));
     }
     return keyterms;
   }
 
+  /**
+   * This function returns a string of NNs from a delim-delimited question text
+   * 
+   * @param questionText
+   *          question text from user that is delim-delimited
+   * @param delim
+   *          what is the delimiter between terms?
+   * @return a string of just NNs
+   */
   private String getNN(String questionText, String delim) {
     String nns = "";
     Set<Map.Entry<Integer, Integer>> spans = ner.getGeneSpans(questionText).entrySet();
@@ -145,14 +232,32 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     return nns.trim();
   }
 
+  /**
+   * @param docText
+   *          document text to match against
+   * @param p
+   *          what pattern to match to the document
+   * @return a multiplier >1 if there is a match
+   */
   private float looseMatchBoost(String docText, Pattern p) {
     boolean m = p.matcher(docText).matches();
-    if (m) {
-      System.out.println("GOT EM");
-    }
-    return (float) (m ? 1.5 : 1);
+    return (float) (m ? LOOSE_MATCH_BOOST : 1);
   }
 
+  /**
+   * This takes the raw documents from a SOLR query and filters it using loose matches and produces
+   * a new rank of a subset of documents accordingly.
+   * 
+   * @param questionText
+   *          question text from user
+   * @param keyterms
+   *          keyterms from keyterm extractor
+   * @param rawRetrievalResults
+   *          a set of documents from SOLR (unfiltered) and of magnitude 'hitListSize'
+   * @param maxReturn
+   *          how many documents to return -- this must be less than 'hitListSize'
+   * @return
+   */
   private List<RetrievalResult> filterDocuments(String questionText, List<Keyterm> keyterms,
           List<RetrievalResult> rawRetrievalResults, int maxReturn) {
     List<RetrievalResult> filteredResults = new ArrayList<RetrievalResult>();
@@ -170,12 +275,12 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
       }
       boosterPatterns.add(Pattern.compile(keytermRegex.trim()));
     }
-    // sort raw
+    // sort raw documents
     Collections.sort(rawRetrievalResults, RetrievalResultComparator);
     for (RetrievalResult result : rawRetrievalResults) {
       RetrievalResult result2 = new RetrievalResult(result.getDocID(), result.getProbability(),
               questionText);
-      // get text
+      // get document text from SOLR
       try {
         String docText = wrapper.getDocText(result2.getDocID());
         for (Pattern p : boosterPatterns) {
@@ -192,17 +297,24 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     filteredResults = filteredResults.subList(0, maxReturn);
     for (int i = 0; i < filteredResults.size(); i++) {
       filteredResults.get(i).setRank(i);
-      System.out.println(" FDOC: " + filteredResults.get(i).getDocID() + " ranked at "
+      System.out.println(" FilteredDocument: " + filteredResults.get(i).getDocID() + " ranked at "
               + filteredResults.get(i).getRank() + " or "
               + (Float) filteredResults.get(i).getProbability());
     }
     return filteredResults;
   }
 
+  /**
+   * Retrieve the raw set of documents from SOLR, using 'hitListSize' as how many we get.
+   * 
+   * @param queryString
+   *          the query string that we had formulated
+   * @return that list of documents
+   */
   protected List<RetrievalResult> retrieveDocuments(String queryString) {
     List<RetrievalResult> result = new ArrayList<RetrievalResult>();
     try {
-      System.out.println(" QQ: " + queryString);
+      System.out.println(" Query: " + queryString);
       SolrQuery query = new SolrQuery();
       query.setQuery(wrapper.escapeQuery(queryString));
       query.setRows(hitListSize);
@@ -214,7 +326,7 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
                 (Float) doc.getFieldValue("score"), queryString);
         r.setRank(i++);
         result.add(r);
-        System.out.println(" DOC: " + doc.getFieldValue("id") + " ranked at " + r.getRank()
+        System.out.println(" Document: " + doc.getFieldValue("id") + " ranked at " + r.getRank()
                 + " or " + (Float) doc.getFieldValue("score"));
       }
     } catch (Exception e) {
@@ -223,6 +335,17 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     return result;
   }
 
+  /**
+   * Generate a query given the question text and keyterms from the keyterm extractor. This will
+   * call wikipedia redirect and big huge thesaurus to expand the keyterms. This will add asterisks
+   * if parameterized and stem if parameterized as well.
+   * 
+   * @param questionText
+   *          question text from user
+   * @param keyterms
+   *          keyterms from keyterm extractor
+   * @return
+   */
   protected String formulateQuery(String questionText, List<Keyterm> keyterms) {
     StringBuffer result = new StringBuffer();
     // get only original keyterms
@@ -232,7 +355,7 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     }
     keyterms = new ArrayList<Keyterm>();
     List<Keyterm> extended = new ArrayList<Keyterm>();
-    System.err.println("0: " + shrunk.toString());
+    System.out.println(" Formulate phase 0: " + shrunk.toString());
     // wikipedia redirect
     try {
       extended = KeytermListExtendor.wikipediaRedirect(shrunk);
@@ -243,8 +366,8 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     for (Keyterm keyterm : extended) {
       keyterms.add(keyterm);
     }
-    System.err.println("1: " + extended.toString());
-    // random website
+    System.out.println(" Formulate phase 1: " + extended.toString());
+    // random website (big huge thesaurus)
     try {
       extended = KeytermListExtendor.KeytermListExtendor(shrunk);
     } catch (IOException e) {
@@ -254,26 +377,26 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
     for (Keyterm keyterm : extended) {
       keyterms.add(keyterm);
     }
-    System.err.println("2: " + extended.toString());
+    System.out.println(" Formulate phase 2: " + extended.toString());
     // add back originals
     for (Keyterm keyterm : shrunk) {
       keyterms.add(keyterm);
     }
     // filter uniques only
+    @SuppressWarnings("unchecked")
     Set<Keyterm> uniques = new HashSet<Keyterm>(Arrays.asList(keyterms.toArray()));
     // remove keyterms of only two letters (except those like L2)
     Pattern twoPattern = Pattern.compile("[A-Z][0-9]");
     for (Keyterm keyterm : uniques) {
       if (keyterm.getText().length() > MINIMUM_KEYTERM_LENGTH
               || twoPattern.matcher(keyterm.getText()).matches()) {
+        // stemming always invokes wildcard
         if (STEM) {
-          System.out
-                  .println(" TRANSFORM: " + keyterm.getText() + " --> " + stem(keyterm.getText()));
+          System.out.println(" Stemming: " + keyterm.getText() + " --> " + stem(keyterm.getText()));
           result.append("\"" + stem(keyterm.getText()) + "*\" ");
-          // System.out.println(" TRANSFORM: " + keyterm.getText() + " --> "
-          // + MorphaStemmer.stemToken(keyterm.getText()));
         } else {
-          if (asterisk) {
+          // are we wildcarding?
+          if (ASTERISK) {
             result.append("\"" + keyterm.getText() + "*\" ");
           } else {
             result.append("\"" + keyterm.getText() + "\" ");
@@ -281,13 +404,21 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
         }
       }
     }
-    // result.append(getNN(questionText, " "));
+    // finally append the original question text, because this sadly improves performance a TON
     result.append(questionText);
+    // prepare query
     String query = result.toString();
-    System.out.println(" QUERY: " + query);
+    System.out.println(" Formulated query: " + query);
     return query;
   }
 
+  /**
+   * Calls the Porter stemmer on strings of at least 'MINIMUM_KEYTERM_LENGTH'
+   * 
+   * @param s
+   *          string to stem
+   * @return the stemmed string
+   */
   private String stem(String s) {
     if (s.length() > MINIMUM_KEYTERM_LENGTH) {
       Stemmer stemmer = new Stemmer();
@@ -298,5 +429,4 @@ public class Team04RetrievalStrategist extends AbstractRetrievalStrategist {
       return s;
     }
   }
-
 }
